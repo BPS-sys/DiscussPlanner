168eba39c9abc384ea51daeda3adad622d135e28
BPS-sys <prgrmsilver@gmail.com>
2024-12-11 15:06:24 +0900

    カウントダウン機能の追加

--- dev/frontend/react_app/src/components/Timer.jsx
+++ dev/frontend/react_app/src/components/Timer.jsx
@@ -1,12 +1,14 @@
 
 import { TimerStringLetter } from "./TimerStringLetter";
 import FacebookCircularProgress from "./TimerCircle";
+import { TimerValue } from "./TimerValue";
 
 export const Timer = () => {
     return (
         <div>
             <TimerStringLetter></TimerStringLetter>
             <FacebookCircularProgress></FacebookCircularProgress>
+            <TimerValue></TimerValue>
         </div>
     );
 };
