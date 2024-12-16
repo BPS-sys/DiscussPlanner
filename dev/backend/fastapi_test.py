1215863640873bb32bf71bb2f75a586730b0534e
BPS-sys <prgrmsilver@gmail.com>
2024-12-16 17:13:15 +0900

    fastapi起動コマンド修正

--- dev/backend/fastapi_test.py
+++ dev/backend/fastapi_test.py
@@ -25,4 +25,4 @@ async def process_transcript(request: Request):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("fastapi_test:app", host="127.0.0.1", port=8000, reload=True)
