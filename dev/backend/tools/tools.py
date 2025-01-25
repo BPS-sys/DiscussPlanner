ad3fc30d1bb69dd59e6c7a3e9efcf11855e212f2
nogi <94681885+nogikun@users.noreply.github.com>
2025-01-25 18:28:30 +0900

    toolの入出力テンプレートを作成

--- dev/backend/tools/tools.py
+++ dev/backend/tools/tools.py
@@ -8,15 +8,33 @@ from langchain_core.tools import tool
 # --- tool functions import ---
 from tools.category import ArgsCategory, tool_category
 from tools.query import QueryVectorstore, query_vectorstore
+from tools.schedule_timetable import (
+    ScheduleConetnts,
+    ScheduleTimeTable,
+    schedule_timetable,
+)
 
 
 # tools
+
 
 class QandATools(BaseModel):
     """
     AIへ質問がされた際に回答を返す際用いるツール
     """
+
     tools: list = Field([QueryVectorstore], description="ツールのリスト")
-    functions: dict = Field({
-        "QueryVectorstore": query_vectorstore
-    }, description="ツールの関数")
+    functions: dict = Field(
+        {"QueryVectorstore": query_vectorstore}, description="ツールの関数"
+    )
+
+
+class ScheduleTimeTableTools(BaseModel):
+    """
+    ミーティングで話し合う内容をスケジュールを作成するツール
+    """
+
+    tools: list = Field([ScheduleTimeTable], description="ツールのリスト")
+    functions: dict = Field(
+        {"ScheduleTimeTable": schedule_timetable}, description="ツールの関数"
+    )
